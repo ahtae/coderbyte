c7862a8aa1fd967e2dadc6016535922371c5eb48
Kristy Li <kristyli20.0@gmail.com>
2020-01-06 13:04:31 -0500

    Complete solution

--- easy/14-Ex-Oh.js
+++ easy/14-Ex-Oh.js
@@ -3,10 +3,18 @@
 // Have the function ExOh(str) take the str parameter being passed and return the string true if there is an equal number of x's and o's, otherwise return the string false. Only these two letters will be entered in the string, no punctuation or numbers. For example: if str is "xooxxxxooxo" then the output should return false because there are 6 x's and 5 o's.
 
 function ExOh(str) {
+  let frequency = {};
 
-  // code goes here
-  return str;
+  if(str.length % 2 !== 0) return false;
 
+  for (let i = 0; i < str.length; i++) {
+    let currentLetter = str[i].toLowerCase();
+
+    if (currentLetter in frequency) frequency[currentLetter]++;
+    else frequency[currentLetter] = 1;
+  }
+
+  return frequency['x'] === frequency['o'];
 }
 
 console.log( ExOh("xooxxo") )
