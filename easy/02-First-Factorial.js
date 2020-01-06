b008c15165ddfd982acc2e8e43b6f29d590a63ec
Kristy Li <kristyli20.0@gmail.com>
2020-01-06 12:24:31 -0500

    Complete solution

--- easy/02-First-Factorial.js
+++ easy/02-First-Factorial.js
@@ -3,10 +3,8 @@
 // have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18.
 
 function FirstFactorial(num) {
-
-  // code goes here
-  return num;
-
+  if (num <= 1) return 1;
+  return num * FirstFactorial(num - 1);
 }
 
 console.log( FirstFactorial( 4 ) )
