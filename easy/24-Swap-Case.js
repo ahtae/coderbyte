14e1799626292623c6505af5a58d9fba299dd74f
Kristy Li <kristyli20.0@gmail.com>
2020-01-10 19:28:24 -0500

    Complete solution

--- easy/24-Swap-Case.js
+++ easy/24-Swap-Case.js
@@ -3,12 +3,22 @@
 // Have the function SwapCase(str) take the str parameter and swap the case of each character.
 // For example: if str is "Hello World" the output should be hELLO wORLD. Let numbers and symbols stay the way they are.
 
+
 function SwapCase(str) {
+  let newStr = "";
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
-  // code goes here
-  return str;
+  for (let i = 0; i < str.length; i++) {
+    let currentLetter = str[i].toLowerCase();
+    let isCapital = (str[i] === str[i].toUpperCase());
 
+    if (alphabet.indexOf(currentLetter) !== -1) isCapital ? newStr += currentLetter.toLowerCase() : newStr += currentLetter.toUpperCase();
+    else newStr += currentLetter;
+  }
+
+  return newStr;
 }
+
 console.log( SwapCase("Hello-LOL") )
 console.log( SwapCase("Sup DUDE!!?") )
 
